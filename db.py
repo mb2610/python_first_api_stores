b8849116e637e0be4f92d952be74870cd39b9112
mb2610 <maissa.baker@live.fr>
2022-10-01 00:12:38 +0200

    mote data to db file

---
+++ db.py
@@ -0,0 +1,18 @@
+stores = {
+    1: {
+        "name": "My Store"
+    }
+}
+
+items = {
+    1: {
+        "name": "Item 1", 
+        "price": 15.99,
+        "store_id": 1
+    },
+    2: {
+        "name": "Item 2", 
+        "price": 15.99,
+        "store_id": 1
+    }
+}
